975ed771c473f1e86cc77b74d1fff790a1a0d52f
Christoffer Pedersen <mail@cstp.dk>
2015-10-20 14:33:51 +0000

    No more pseudo terminal, it caused more havoc than it good
    With great pseudo powers comes great responsibilities, apparently

--- orchid/pipeline.go
+++ orchid/pipeline.go
@@ -127,7 +127,7 @@ func buildExecutable(path string, executable Executable, machines []Machine, log
 		}
 
 		sshCommand := fmt.Sprintf(
-			"ssh -tt -o 'StrictHostKeyChecking no' %s@%s -p %s -i %s 'bash -s' -- < %s %s",
+			"ssh -t -o 'StrictHostKeyChecking no' %s@%s -p %s -i %s 'bash -s' -- < %s %s",
 			machine.User,
 			machine.Address,
 			machine.Port,
